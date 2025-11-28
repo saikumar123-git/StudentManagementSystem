6e91e825a9787acbf4e61cc4c5c047295cabfbc2
saikumar123-git <saikantamreddi124@gmail.com>
2025-11-28 14:43:29 +0530

    Delete student (1) (1)/student (1)/student directory

--- student (1) (1)/student (1)/student/src/main/java/com/example/student/repo/RegistrationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.student.repo;
-
-import com.example.student.api.Registration;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegistrationRepository extends JpaRepository<Registration,Integer> {
-    Registration findByUsername(String username);
-
-}
